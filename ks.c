4bb17a6459eebf266b5ec303b0c63c58f10682e5
void <skandians@proton.me>
2026-02-15 20:36:47 -0500

    2nd

--- ks.c
+++ ks.c
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "header.h"
-void bit_disp(uint64_t bits){
+void bit_disp(uint64_t arg, uint8_t size){
 	uint8_t bit;
-	for (uint16_t j = 0; j < 64; j++){
+	uint16_t i = 1;
+	for (uint16_t j = 0; j < (size); j++, i++){
 		bit = 0;
-		if (bits & (1L<<j)){
+		if (arg & (1L<<j)){
 			bit = 1;
 		}
 		printf("%d",bit);
+		if ((i%8) == 0){
+			printf("\t");
+		}
+		
 	} 
 	printf("\n");
 }
@@ -33,18 +38,35 @@ uint64_t keyshed(uint16_t n, uint64_t key1){
 			44,49,39,56,34,53,
 			46,42,50,36,29,32};
 	uint64_t key2 = 0;
-	printf("%d \n", sizeof(pc1));
-	if (n == 1){
-		for ( uint16_t j  = 0; j < 56; j++){
-			if (key1 & (1L << (pc1[j]-1))){
-				key2 |= 1L << j;
-			}
+
+	//PC-1
+	for ( uint16_t j  = 0; j < 56; j++){
+		if (key1 & (1L << (pc1[j]-1))){
+			key2 |= 1L << j;
 		}
 	}
-	printf("%Lb \n",key1);
-	bit_disp(key1);
-	printf("%Lb \n",key2);
-	bit_disp(key2);
+	bit_disp(key1,64);
+	bit_disp(key2,64);
+
+	//Initial Seperation //28 each	
+	uint32_t C = 0;
+	uint32_t D = 0;
+	
+	for (uint16_t j = 0; j < 28; j++){
+		if (key1 & ( 1L << (pc1[j]-1) )){
+			C |= 1L << j;
+		}
+	}
+	for (uint16_t j = 0; j < 28; j++) {
+		if (key1 & (1L << (pc1[j+28]-1) )){
+			D |= 1L << j;
+		}
+	}
+	D = D >> 4 ; //Bug of sorts
+	printf("\n");
+	bit_disp(C,32);
+	bit_disp(D,32);
+
 
 	return 0;
 	
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include "header.h"
 int main(){
-	uint64_t num1 = 257;
+	uint64_t num1 = 25777777;
 	keyshed(1,num1);
 	uint32_t pc1[] = {57,49,41,33,25,17,9,
                         1,58,50,42,34,26,18,
@@ -14,14 +14,15 @@ int main(){
                         14,6,61,53,45,37,29,
                         21,13,5,28,20,12,4};
 	uint64_t key = 0;
-	
 	//Algo Test	
-	for ( uint16_t j  = 0; j < 56; j++){
-                        if (num1 & (1L << (pc1[j]-1))){
-                                key |= 1L << j;
-                        }
-                }
-	bit_disp(num1);
-	bit_disp(key);
+	//for ( uint16_t j  = 0; j < 56; j++){
+        //                if (num1 & (1L << (pc1[j]-1))){
+        //                        key |= 1L << j;
+        //                }
+        //        }
+	//bit_disp(num1,64);
+	//bit_disp(key,64);
+
+
 	return 0;
 }
