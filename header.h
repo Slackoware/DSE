f83d71e561bb7013e4c87e039de0fd566cb671ea
void <skandians@proton.me>
2026-02-26 20:52:01 -0500

    First Draft, Not tested bugs left

--- header.h
+++ header.h
@@ -1,9 +1,12 @@
 #ifndef guard
 #define guard
+#include <stdint.h>
 void bit_disp(uint64_t arg, uint8_t size);
 uint64_t keyshed(uint64_t key1, uint64_t keys[6]);
 uint64_t pc_2(uint32_t c, uint32_t d, uint32_t pc2[46]);
 uint32_t function(uint32_t r, uint64_t key);
 uint64_t bit_addition(uint64_t a, uint64_t b);
+uint64_t des1(uint64_t text, uint64_t keys[6]);
+uint64_t des(uint64_t text, uint64_t keys[6]);
 
 #endif
